0db0822b24ec2de0b0526e8b8090d8cc854deea3
Savvy Sahai <71918602+SavvySahai@users.noreply.github.com>
2023-06-22 14:24:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-//st.title("Trying the module")
-//st.header('First Line')
-//st.text('Hopefully this has displayed without an error.')
-//st.text('Using streamlit is really easy.')
-//st.text('And if I am stuck I can always refer the docs!')
+#st.title("Trying the module")
+#st.header('First Line')
+#st.text('Hopefully this has displayed without an error.')
+#st.text('Using streamlit is really easy.')
+#st.text('And if I am stuck I can always refer the docs!')
 
 st.title('My Parents New Healthy Diner')
 st.header('Breakfast Menu')
