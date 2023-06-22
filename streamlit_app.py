6b721485ee970e80b385fa847077648cd926b165
Savvy Sahai <71918602+SavvySahai@users.noreply.github.com>
2023-06-22 13:03:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 
 streamlit.title("Trying the module")
-streamlit.header("First Line")
-streamlit.text("Hopefully this has displayed without an error")
-streamlit.text("Using streamlit is really easy")
-streamlit.text("And if I am stuck I can always refer the docs!")
+
+streamlit.header('First Line')
+
+streamlit.text('Hopefully this has displayed without an error')
+streamlit.text('Using streamlit is really easy')
+streamlit.text('And if I am stuck I can always refer the docs!')
