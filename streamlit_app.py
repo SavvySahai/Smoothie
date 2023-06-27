0f3c448beefb71eef07884faa485ba27f1f1c588
Savvy Sahai <71918602+SavvySahai@users.noreply.github.com>
2023-06-27 13:58:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,6 @@ fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # DISPLAYING IN THE TABULAR FORM
-# my_fruit_list = my_fruit_list.set_index('Fruit')
 st.dataframe(fruits_to_show)
 
 # CREATING FUNCTION FOR FRUITYVICE DATA
